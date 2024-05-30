3676502bc8e3de2d6d0909db31bedf6b5c10d750
zakirhussain-s <s.zakirhussain02@gmail.com>
2024-05-30 21:16:35 +0530

    Lazy loading added

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -1,13 +1,18 @@
 public class Singleton {
 
     // Step 1: Create a private static variable to hold the single instance of the class
-    private static Singleton instance = new Singleton();
+    // assiging Null will give room for lazy loading
+    private static Singleton instance = null;
 
     // Step 2: Make the constructor private so no one can instantiate the class
     private Singleton(){}
 
     // Step 3: Provide a public static method to get the instance of the class
     public static Singleton getInstance(){
+//        loads the instance only once, this will fetch only when needed and once
+        if (instance == null)
+            instance = new Singleton();
+
         return instance;
     }
 }
