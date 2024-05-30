0b647fa5c49d0cb023f2d3ba28f0e9d6de2f0f83
zakirhussain-s <s.zakirhussain02@gmail.com>
2024-05-30 20:57:18 +0530

    included main class

---
+++ Singleton/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
+
+        Singleton anotherSingleton = Singleton.getInstance();
+        System.out.println(anotherSingleton);
+
+        if (singleton == anotherSingleton)
+            System.out.println("Both references point to the same instance.");
+        else
+            System.out.println("Different instances exist.");
+    }
+    }
