d1ba55bd9eaa96a4b60d0e173c55975298a327f3
zakirhussain-s <s.zakirhussain02@gmail.com>
2024-08-07 20:22:12 +0530

    Abstracy Factory Method Pattern

--- AbstractFactoryMethod/src/Main.java
+++ AbstractFactoryMethod/src/Main.java
@@ -7,5 +7,17 @@ public class Main {
 //      Family of Products: When you need to create groups of related objects (e.g., different types of UI elements for different operating systems).
 //      Consistency:Ensures that the related objects are compatible with each other.
 //      Decoupling: Decouples the client code from the specific classes of products it uses.
+
+        GUIFactory factory;
+
+        String os = "Mac";
+
+        if(os.equals("Windows"))
+            factory = new WindowsFactory();
+        else
+            factory = new MacOSFactory();
+
+        Application app = new Application(factory);
+        app.paint();
     }
 }
